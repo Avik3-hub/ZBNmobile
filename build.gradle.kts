78c021ab76733ced256049dade903eb1d36d633a
Avik3-hub <sergey.nagibin55@gmail.com>
2026-08-08 23:23:57 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,3 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-android {
-    ...
-    applicationVariants.all {
-        outputs.all {
-            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
-            output.outputFileName = "ZBNmobile_${buildType.name}.apk"
-        }
-    }
-}
