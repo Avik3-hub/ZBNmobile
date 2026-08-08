bea1e6a3dbcd45bf302928f276e1f0414d0a81a4
Avik3-hub <sergey.nagibin55@gmail.com>
2026-08-08 22:17:51 +0300

    Create ZbnFlightDescriptor.kt

---
+++ app/src/main/java/com/example/zbnreader/ZbnFlightDescriptor.kt
@@ -0,0 +1,19 @@
+package com.example.zbn
+
+data class ZbnFlightDescriptor(
+    val sectorIndex: Int,
+    val type: Int,
+    val startAddress: Int,
+    val metadataRaw: ByteArray,
+    val subBlockIndex: Int,
+    val isValidCrc: Boolean
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as ZbnFlightDescriptor
+        return startAddress == other.startAddress && sectorIndex == other.sectorIndex
+    }
+
+    override fun hashCode(): Int = startAddress.hashCode()
+}
