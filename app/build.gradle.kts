705ec9afdf930f78cd2fdf65bab07002fc79e846
Avik3-hub <sergey.nagibin55@gmail.com>
2026-08-08 14:08:40 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,5 +28,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("com.github.mik3y:usb-serial-for-android:3.8.0")
+    implementation("com.github.mik3y:usb-serial-for-android:3.9.0")
 }
