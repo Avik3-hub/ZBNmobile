ede35e86a76dc4622d93223c8abfd426bf340b02
Avik3-hub <sergey.nagibin55@gmail.com>
2026-08-08 18:48:34 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/zbnreader/MainActivity.kt
+++ app/src/main/java/com/example/zbnreader/MainActivity.kt
@@ -5,6 +5,7 @@ import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
+import android.content.Intent
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ProgressBar
@@ -31,6 +32,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+val btnSettings = findViewById<Button>(R.id.btnSettings)
+btnSettings.setOnClickListener {
+    val intent = Intent(this, SettingsActivity::class.java)
+    startActivity(intent)
+}
 
         val root = LinearLayout(this).apply {
             orientation = LinearLayout.VERTICAL
