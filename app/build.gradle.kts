95a9da54b6503a8a5fae2cfea929e4ae08e347a3
Avik3-hub <sergey.nagibin55@gmail.com>
2026-08-15 10:00:05 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,22 +13,27 @@ android {
         targetSdk = 34
         versionCode = 2
         versionName = "1.1"
-    } // <-- Закрывающая скобка для defaultConfig
-
-    applicationVariants.all {
-        val variant = this
-        variant.outputs.all {
-            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
-            output.outputFileName = "app-debug.apk"
-        }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        // Apache POI 5.2.3 требует минимум Java 11
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+    
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
+    }
+
+    // Исключаем конфликтующие файлы метаданных Apache POI при сборке APK
+    packaging {
+        resources.excludes += "META-INF/DEPENDENCIES"
+        resources.excludes += "META-INF/LICENSE"
+        resources.excludes += "META-INF/LICENSE.txt"
+        resources.excludes += "META-INF/NOTICE"
+        resources.excludes += "META-INF/NOTICE.txt"
+        resources.excludes += "META-INF/ASL2.0"
+        resources.excludes += "META-INF/LGPL2.1"
     }
 }
 
@@ -37,7 +42,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.github.mik3y:usb-serial-for-android:3.9.0")
-implementation("org.apache.poi:poi-ooxml:5.2.3")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    
     // Корутины для чтения RS-422 и записи в файл в фоновом потоке
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
     // lifecycleScope для безопасного запуска задач из Activity
